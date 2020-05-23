e3285809296cc9fbad91182e20e8e9f779b3b6c0
Wiking <p.polcik1@gmail.com>
2020-05-23 16:20:03 +0200

    Fixing waypoints issue

--- app/components/layout.js
+++ app/components/layout.js
@@ -29,10 +29,10 @@ const Layout = (props) => {
                 <link rel="stylesheet" href="/static/styles/styles.css"></link>
                 <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css" media="print" onload="this.media='all'"></link>
                 <link rel="shortcut icon" type="image/png" href="/static/images/Logo_light.png" />
-                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
                 <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
                 <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-                <script defer src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js" />
+                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js" />
             </Head>
             <Navbar styling={props.navbar} social={props.content.social} content={props.content.navbar} colorSchema={colorSchema} />
             <div className="container px-0 mx-0">
